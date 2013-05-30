8c00272612983cf66342fd02bead701176333acc
Mark Lamley <mark.lamley@globalrelay.net>
2013-05-30 13:10:12 -0700

    Fixed deploy

--- nlog-deploy.bat
+++ nlog-deploy.bat
@@ -1,7 +1,9 @@
 
 
 IF %1.==. GOTO Missing
-.nuget\NuGet.exe push DotNetFlumeNG.Client.NLog.%1.nupkg -s https://nuget.globalrelay.net/ 6467ce88-104a-4d1b-9068-db0dba810b93
+.nuget\NuGet.exe push DotNetFlumeNG.Client.NLog.%1.nupkg
+.nuget\NuGet.exe push DotNetFlumeNG.Client.NLog.%1.symbols.nupkg
+
 GOTO End
 
 
